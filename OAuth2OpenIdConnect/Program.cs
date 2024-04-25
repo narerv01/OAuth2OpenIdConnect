f1a3da8a65f5e56f4e6d324849919d381950c2b0
narerv01 <narerv01@easv365.dk>
2024-04-25 21:24:11 +0200

    Add project files.

---
+++ OAuth2OpenIdConnect/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using OAuth2OpenIdConnect.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<CacheService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{ 
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ OAuth2OpenIdConnect/Services/CacheService.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace OAuth2OpenIdConnect.Services
+{
+    public class CacheService
+    {
+
+
+        private readonly Dictionary<string, string> _cache = new Dictionary<string, string>();
+
+        public void AddToCache(string key, string value)
+        {
+            _cache[key] = value;
+        }
+
+        public string GetFromCache(string key)
+        {
+            return _cache.TryGetValue(key, out string value) ? value : null;
+        }
+    }
+}
